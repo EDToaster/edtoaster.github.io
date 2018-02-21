8853a55751fc62dc91e65f4109ea7a2b6491bc36
ClassicPork <classicpork@gmail.com>
2018-02-21 00:26:32 -0500

    Added triple view, mirrored

--- pendulum/pendulum.js
+++ pendulum/pendulum.js
@@ -6,15 +6,16 @@ let factor = 10;
 let upf = 1;
 
 let curr = null;
+let opp = null;
 
 let coordX1 = 0;
 let coordY1 = 0;
 
 function setup() {
-  var canvas = createCanvas(plotw * factor * 2, ploth * factor);
+  var canvas = createCanvas(plotw * factor * 3, ploth * factor);
 
-  let delta1 = TWO_PI / plotw;
-  let delta2 = TWO_PI / ploth;
+  let delta1 = TWO_PI / (plotw - 1);
+  let delta2 = TWO_PI / (ploth - 1);
 
   for (var i = 0; i < plotw; i++) {
     for (var j = 0; j < ploth; j++) {
@@ -41,12 +42,14 @@ function draw() {
 
 
   if (curr != null) {
-    curr.display(width / 2, 0, width / 2, height);
+    curr.display(width / 3, 0, width / 3, height);
+    opp.display(width / 3 * 2, 0, width / 3, height);
 
     noFill();
     stroke(0);
     strokeWeight(factor / 5);
     rect(coordX1 * factor, coordY1 * factor, factor, factor);
+    rect(coordX2 * factor, coordY2 * factor, factor, factor);
   }
 
 }
@@ -59,6 +62,9 @@ function mousePressed() {
   } else {
     coordX1 = i;
     coordY1 = j;
+    coordX2 = plotw - 1 - i;
+    coordY2 = ploth - 1 - j;
     curr = arr[coordY1 * plotw + coordX1];
+    opp = arr[coordY2 * plotw + coordX2];
   }
 }
