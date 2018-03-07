3cf5ca88949e1f2b9a3551e48161be9d5ad7609e
ClassicPork <classicpork@gmail.com>
2018-03-07 01:04:18 -0500

    Added variable hidden nodes, stats sidebar

--- javascript/doodle/sketch.js
+++ javascript/doodle/sketch.js
@@ -82,9 +82,10 @@ function split_arr(arr, n) {
   }
   return res;
 }
+var font;
 
 function preload() {
-
+  font = loadFont("data/Anonymous.ttf");
 
   for (let i = 0; i < files.length; i++) {
     let e = files[i];
@@ -111,13 +112,17 @@ const total = 20;
 let size;
 let nn;
 
-const hidden_nodes = 100;
+let hidden_nodes;
+const def_nodes = 100;
 
 function setup() {
-  createCanvas(800, 460);
+  createCanvas(1200, 460);
   pimage = createImage(28, 28);
   imagearr = new Array(filesize);
   imagearr.fill(0);
+
+  let val = parseInt(getfromname('n'));
+  hidden_nodes = val ? (val < 1 ? def_nodes : val) : def_nodes;
 
   size = height / total;
   last = createGraphics(400, 400);
@@ -133,6 +138,8 @@ let last;
 let trainings = 0;
 const periter = 50;
 
+let dotrain = false;
+
 function draw() {
   //training
   background(0);
@@ -142,21 +149,26 @@ function draw() {
   if (mouseIsPressed) {
     if (mouseButton == LEFT)
       last.line(pmouseX, pmouseY, mouseX, mouseY);
-    else {
-      last.background(0);
-    }
   }
-
-  train(periter);
-  trainings += periter;
-
+  if (dotrain) {
+    train(periter);
+    trainings += periter;
+  }
   img = last.get();
   img.resize(28, 28);
 
-  image(last, 0, 0, width / 2, height - 60);
-  image(img, width / 2, 0, width / 2, height - 60);
+  image(last, 0, 0, width / 3, height - 60);
+  image(img, width / 3, 0, width / 3, height - 60);
   testImage(img);
 
+}
+
+function mousePressed() {
+  if (mouseButton == CENTER) {
+    last.background(0);
+  } else if (mouseButton == RIGHT) {
+    dotrain = !dotrain;
+  }
 }
 
 function testImage(img) {
@@ -165,14 +177,31 @@ function testImage(img) {
   for (let i = 0; i < img.pixels.length; i += 4) {
     data.push(img.pixels[i] / 255.0);
   }
-  let guess = nn.predict(data);
-  let ind = guess.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+  let guesses = nn.predict(data);
+  let mappedguess = [];
+  for (let i = 0; i < guesses.length; i++) {
+    mappedguess.push({
+      label: files[i].name,
+      value: guesses[i]
+    });
+  }
+
+  mappedguess.sort(function(a, b) {
+    return b.value - a.value;
+  });
+
   strokeWeight(2);
   stroke(0);
+  textFont(font);
   textSize(30);
   fill(255);
-  text(files[ind].name, 0, height);
-  text(trainings + ", " + guess[ind], 0, height - 30);
+  text(mappedguess[0].label, 0, height);
+  text(trainings + ": " + hidden_nodes, 0, height - 30);
+
+  for (let i = 0; i < 10; i++) {
+    textSize(10);
+    text(mappedguess[i].label.padEnd(30) + ": " + mappedguess[i].value, width / 3 * 2, 30 * (i + 1));
+  }
 }
 
 function test(n) {
@@ -203,4 +232,12 @@ function train(n) {
     let target = e.target;
     nn.train(data, target);
   }
+}
+
+function getfromname(name) {
+  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+    return decodeURIComponent(name[1]);
+}
+document.oncontextmenu = function() {
+  return false;
 }
