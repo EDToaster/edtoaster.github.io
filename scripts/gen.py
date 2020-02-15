5ec8bf600f355d8569317b892057a1bcfb398614
EDToaster <elongateddanishtoaster@gmail.com>
2020-02-15 02:24:08 -0500

    stuff

---
+++ scripts/gen.py
@@ -0,0 +1,14 @@
+# Generates a background for my website (paper texture)
+from PIL import Image
+import random
+ 
+def random_grey(lo, hi):
+    return (random.randint(lo, hi), random.randint(lo, hi), random.randint(lo, hi))
+
+dimensions = (256, 256)
+array = [random_grey(0xea, 0xf5) for i in range(dimensions[0] * dimensions[1])]
+
+img = Image.new("RGB", size=dimensions)
+img.putdata(array)
+
+img.save("../assets/background.png")
