721b3fe4ebcc0f4aaa25d11dd35ed1e7cc78652e
EDToaster <elongateddanishtoaster@gmail.com>
2020-02-15 22:31:41 -0500

    update

--- scripts/gen.py
+++ scripts/gen.py
@@ -6,7 +6,8 @@ def random_grey(lo, hi):
     return (random.randint(lo, hi), random.randint(lo, hi), random.randint(lo, hi))
 
 dimensions = (256, 256)
-array = [random_grey(0xea, 0xf5) for i in range(dimensions[0] * dimensions[1])]
+# array = [random_grey(0xea, 0xf5) for i in range(dimensions[0] * dimensions[1])]
+array = [random_grey(0xef, 0xfa) for i in range(dimensions[0] * dimensions[1])]
 
 img = Image.new("RGB", size=dimensions)
 img.putdata(array)
